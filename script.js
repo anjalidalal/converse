188818a3943ca39ed897ef2f45c85ba9f15ae125
anjalidalal <anjalimaria2002@gmail.com>
2022-03-28 20:20:04 +0530

    Landing page responsive view

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const belgium = document.getElementById
+const belgium = document.getElementById("belgium")
+const belgiumDiv = document.querySelector(".belgium")
+const header1 = document.querySelector(".header1");
+const countryContainer = document.querySelector(".countryContainer")
+
+
+function displayBelDiv() {
+    header1.style.display = "none";
+    countryContainer.style.display = "none";
+    belgiumDiv.style.display = "unset";
+
+    console.log(1)
+}
+
+belgium.addEventListener("click", displayBelDiv)
